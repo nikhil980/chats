99821b4b3da07874e8b4a63c7f3e70733914263f
nikhil980 <154825911+nikhil980@users.noreply.github.com>
2024-12-28 20:26:46 +0530

    Update useSign.js

--- src/hooks/useSign.js
+++ src/hooks/useSign.js
@@ -24,7 +24,7 @@ const useSign = () => {
     setloading(true);
     try
     {
-      const res=await fetch("http://localhost:5000/api/auth/signup",{
+      const res=await fetch("https://chats-backend-sgl1.onrender.com/api/auth/signup",{
         method:"POST",
         headers:{"Content-Type":"application/json"},
         body:JSON.stringify({
